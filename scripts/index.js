583e5ea3f8878e970cf31029fd5fb709b5ad5c22
Dmitry Savelev <dsavelev1703@yandex.ru>
2022-05-22 00:32:42 +0300

    исправил все замечания ревьюера

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,26 @@
 const popup = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__icon');
+
 function togglePopup() {
-  popup.classList.toggle('popup_opened');
+  if (popup.classList.contains('popup_opened')) {
+    popup.classList.remove('popup_opened')
+  } else {
+    popup.classList.add('popup_opened')
+  }
 }
 
 editButton.addEventListener('click', togglePopup);
 closeButton.addEventListener('click', togglePopup);
-
-popup.addEventListener('click', function (event) {
-  if (event.target === event.currentTarget) {
-    togglePopup();
-  }
-});
 
 // Редактирование имени и информации о себе
 
 const formElement = document.querySelector('.popup__form');
 const nameInput = document.querySelector('.popup__name-input');
 const jobInput = document.querySelector('.popup__job-input');
+const save = document.querySelector('.popup__button');
 
-document.querySelector('.popup__button').onclick = function (e) {
+save.onclick = function (e) {
   e.preventDefault();
   document.querySelector('.profile__name').textContent = nameInput.value;
   document.querySelector('.profile__job').textContent = jobInput.value;
