17d97bf7ede5271f77bdcb46d565ce3db12b90fa
Dmitry Savelev <dsavelev1703@yandex.ru>
2022-05-29 00:20:51 +0300

    перед 4ой отправкой

--- scripts/index.js
+++ scripts/index.js
@@ -4,18 +4,15 @@ const closeButton = document.querySelector('.popup__button-close');
 const formElement = document.querySelector('.popup__form');
 const nameInput = document.querySelector('.popup__name-input');
 const jobInput = document.querySelector('.popup__job-input');
-const save = document.querySelector('.popup__button');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 
 function togglePopup() {
+  popup.classList.toggle('popup_opened');
   if (popup.classList.contains('popup_opened')) {
-    popup.classList.remove('popup_opened');
-  } else {
-    popup.classList.add('popup_opened');
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
-  }
+}
 }
 
 editButton.addEventListener('click', togglePopup);
@@ -23,7 +20,7 @@ closeButton.addEventListener('click', togglePopup);
 
 // Редактирование имени и информации о себе
 
-save.addEventListener('click', function (e) {
+formElement.addEventListener('submit', function (e) {
   e.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
