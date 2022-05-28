af75bdd4c772ab76b9e9ebea050f46cb272e34e8
Dmitry Savelev <dsavelev1703@yandex.ru>
2022-05-28 15:44:12 +0300

    финальные правки

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,20 @@
 const popup = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button');
-const closeButton = document.querySelector('.popup__icon');
+const closeButton = document.querySelector('.popup__button-close');
+const formElement = document.querySelector('.popup__form');
+const nameInput = document.querySelector('.popup__name-input');
+const jobInput = document.querySelector('.popup__job-input');
+const save = document.querySelector('.popup__button');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
 
 function togglePopup() {
   if (popup.classList.contains('popup_opened')) {
-    popup.classList.remove('popup_opened')
+    popup.classList.remove('popup_opened');
   } else {
-    popup.classList.add('popup_opened')
+    popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
   }
 }
 
@@ -15,14 +23,10 @@ closeButton.addEventListener('click', togglePopup);
 
 // Редактирование имени и информации о себе
 
-const formElement = document.querySelector('.popup__form');
-const nameInput = document.querySelector('.popup__name-input');
-const jobInput = document.querySelector('.popup__job-input');
-const save = document.querySelector('.popup__button');
-
-save.onclick = function (e) {
+save.addEventListener('click', function (e) {
   e.preventDefault();
-  document.querySelector('.profile__name').textContent = nameInput.value;
-  document.querySelector('.profile__job').textContent = jobInput.value;
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
   togglePopup();
 }
+)
